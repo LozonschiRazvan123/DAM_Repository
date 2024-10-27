7be232e9aad865698b93a6e8f56edc3ecc2b6d3e
LOZONSCHI CONSTANTIN-RAZVAN <310440501RSL211122@feaa.uaic.ro>
2024-10-27 18:54:03 +0200

    27/10/2024

---
+++ src/main/java/org/example/Controller/UserController.java
@@ -0,0 +1,14 @@
+package org.example.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping(value = "/api/user")
+    public String user()
+    {
+        return "Welcome";
+    }
+}
