c26505eccb332f31ae0485109710b42b6c2f9648
edy_e <podosu.edy@gmail.com>
2024-10-29 21:16:35 +0200

    29/10/2024-domain model

---
+++ src/main/java/org/example/Model/Produs.java
@@ -0,0 +1,21 @@
+package org.example.Model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Produs {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idProdus;
+
+    private String nume;
+    private String categorie;
+    private double pretVanzare;
+    private double pretAchizitie;
+    private int stoc;
+
+    @ManyToOne
+    private Furnizor furnizor;
+
+    // getters și setters
+}
