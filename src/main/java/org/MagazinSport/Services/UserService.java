6009435a69dae0ef1a8f92fde0777d5bb27bcbdc
LOZONSCHI CONSTANTIN-RAZVAN <310440501RSL211122@feaa.uaic.ro>
2024-12-22 12:25:24 +0200

    update authorization

--- src/main/java/org/MagazinSport/Services/UserService.java
+++ src/main/java/org/MagazinSport/Services/UserService.java
@@ -6,6 +6,8 @@ import org.MagazinSport.Model.User;
 import org.MagazinSport.Repository.UserRepository;
 import org.MagazinSport.Role.Role;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -13,6 +15,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Service
 public class UserService implements UserDetailsService {
@@ -62,7 +67,17 @@ public class UserService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByUsername(username)
+        User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.name()))
+                .collect(Collectors.toList());
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                authorities
+        );
     }
 }
